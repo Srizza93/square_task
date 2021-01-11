bdabed8f8c2e1a0ba301a472a4538d847b704d48
Simone <63718887+Srizza93@users.noreply.github.com>
2021-01-11 23:44:14 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let event;
+let squarePos = document.getElementById("square");
+function mouseCoordinates(event) {
+  let x = event.clientX;
+  let y = event.clientY;
+  squarePos.style.position = "absolute";
+  squarePos.style.left = x + "px";
+  squarePos.style.top = y + "px";
+  squarePos.style.width = "80px";
+  squarePos.style.height = "80px";
+}
+
+function mouseOut(event) {
+  squarePos.style.width = "50px";
+  squarePos.style.height = "50px";
+}
+
+
