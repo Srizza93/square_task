c45c3e382d4320434a918bf8e8eb6bbf0fd347b1
Simone <63718887+Srizza93@users.noreply.github.com>
2021-01-23 12:54:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-document.addEventListener("mouseover", myFunction);
-document.addEventListener("mouseout", outFunction);
+var coordinates_module = (function() {
+  'use strict';
+  
+  var squarePos = document.getElementById("square");
+  var event = this.event;
+  
+  function myFunction(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    squarePos.style.position = "absolute";
+    squarePos.style.left = x + "px";
+    squarePos.style.top = y + "px";
+    squarePos.style.width = "100px";
+    squarePos.style.height = "100px";
+  }
+  
+  function outFunction() {
+    squarePos.style.width = "50px";
+    squarePos.style.height = "50px";
+  }
+  
+  return {
+    movement: function() {
+      document.addEventListener("mouseover", myFunction);
+      document.addEventListener("mouseout", outFunction);
+    }
+  }
+  
+})();
 
-let event;
-let squarePos = document.getElementById("square");
-function myFunction(event) {
-  let x = event.clientX;
-  let y = event.clientY;
-  squarePos.style.position = "absolute";
-  squarePos.style.left = x + "px";
-  squarePos.style.top = y + "px";
-  squarePos.style.width = "100px";
-  squarePos.style.height = "100px";
-}
-
-function outFunction() {
-  squarePos.style.width = "50px";
-  squarePos.style.height = "50px";
-}
-
-
+coordinates_module.movement();
