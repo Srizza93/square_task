62990b36f11aeba6ceabbc89fc1c02ededfae16e
Simone <63718887+Srizza93@users.noreply.github.com>
2021-01-24 22:45:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let squarePos = document.getElementById("square");
-const space = document.getElementsByTagName("body");
-
 function mouse_move(e) {
+  let squarePos = document.getElementById("square");
+  const space = document.getElementsByTagName("body");
   let x = e.clientX, y = e.clientY;
   squarePos.style.position = "absolute";
   squarePos.style.left = x + 'px';
