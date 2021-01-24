26bfcc621e7bb3f5268901c9896b4e231e984ded
Simone <63718887+Srizza93@users.noreply.github.com>
2021-01-24 22:36:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-let coordinates_module = (function() {  
-  let squarePos = document.getElementById("square");
-  let mouse_on = false, x = 0, y = 0;
-  const space = document.getElementsByTagName("body");
-  
-  function mouse_down(event) {
-    x = event.clientX;
-    y = event.clientY;
-    mouse_on = true;
-    console.log("DETECTED", mouse_on);
+let squarePos = document.getElementById("square");
+let mouse_on = false;
+const space = document.getElementsByTagName("body");
+
+function mouse_move(e) {
+  let x = e.clientX, y = e.clientY;
+  console.log("Detected");
   }
   
-  function mouse_move() {
+function mouse_up() {
   }
   
-  function mouse_up() {
-  }
-  
-  return {
-    movement: function(event) {
-      space.addEventListener("mouseover", mouse_down());
-    }
-  }
-  
-})();
+ 
