2ffb8f975b36a39100b3df3337523405c4379256
Simone <63718887+Srizza93@users.noreply.github.com>
2021-01-25 18:44:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 var my_module = (function() {
+    let x = event.clientX, y = event.clientY;
+    let squarePos = document.getElementById("square");
+
     function mouse_move(event) {
-        let squarePos = document.getElementById("square");
         const space = document.getElementsByTagName("body");
-        let x = event.clientX, y = event.clientY;
         squarePos.style.position = "absolute";
         squarePos.style.left = x + 'px';
         squarePos.style.top = y + 'px';
-        console.log("Detected");
+        console.log("x " + x,"y " + y);
+        }
+
+        function screen_limit() {
+            let screen_width = document.documentElement.clientWidth,
+            screen_height = document.documentElement.clientHeight;
+            console.log("height " + screen_height,"width " + screen_width);
+            if (x >= screen_width - 51) {
+                squarePos.style.left = (screen_width - 51) + 'px';
+                console.log('HERE');
+            }
+            if (y >= screen_height - 51) {
+                squarePos.style.top = (screen_height - 51) + 'px';
+                console.log('HERE');
+            }
         }
 
         return {
-            output:mouse_move(event)
+            output:mouse_move(event),
+            output:screen_limit()
         };
 })
