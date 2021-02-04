741fc5219f675b563dc4aa6e11c57e24b5051b50
Simone <63718887+Srizza93@users.noreply.github.com>
2021-02-04 22:13:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class MyClass {
-
+    
     constructor(square_pos, screen_width, screen_height) {
         this.square_pos = square_pos;
         this.screen_width = screen_width;
@@ -12,11 +12,28 @@ class MyClass {
         square_pos.style.top = event.clientY + 'px';
     } 
     
+    screen() {
+        let space = document.getElementsByClassName("box")[0];
+        let margin_top = window.getComputedStyle(space).marginTop;
+        let margin_top_round = Math.round(Number(margin_top.substring(0,margin_top.indexOf("p"))));
+        if (Number(square_pos.style.left.substring(0,square_pos.style.left.indexOf("p"))) >= (window.innerWidth - screen_width) / 2 + screen_width - square_pos.offsetWidth)
+            square_pos.style.left = (window.innerWidth - screen_width) / 2 + screen_width - square_pos.offsetWidth + 'px';
+        if (Number(square_pos.style.left.substring(0,square_pos.style.left.indexOf("p"))) <= (window.innerWidth - screen_width) / 2) 
+            square_pos.style.left = (window.innerWidth - screen_width) / 2 + 'px';
+        if (Number(square_pos.style.top.substring(0,square_pos.style.top.indexOf("p"))) >= margin_top_round + space.offsetHeight - square_pos.offsetHeight)
+            square_pos.style.top = margin_top_round + space.offsetHeight - square_pos.offsetHeight + 'px';
+        if (Number(square_pos.style.top.substring(0,square_pos.style.top.indexOf("p"))) <= margin_top_round)
+            square_pos.style.top = margin_top_round + 'px';
+    }
+    
 }
 
-let square_pos = document.getElementById("square"),
-screen_width = document.documentElement.clientWidth,
-screen_height = document.documentElement.clientHeight;
+let square_pos = document.getElementById("square");
+let space = document.getElementsByClassName("box")[0];
+console.log(space.style.margin);
+screen_width = space.offsetWidth,
+screen_height = space.offsetHeight;
 let action = new MyClass(square_pos, screen_width, screen_height);
-window.onmousemove = action.coords;
+space.addEventListener("mousemove", action.coords);
+
 
