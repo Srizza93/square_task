32086d923c037686953dd13bf38484f48476ac09
Simone <63718887+Srizza93@users.noreply.github.com>
2021-02-11 23:25:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,46 @@ class Square_run {
         this.margin_bottom = this.margin_top + this.box.offsetHeight;
         this.margin_left =  (window.innerWidth - this.box_width) / 2;
         this.margin_right = window.innerWidth - this.margin_left;
-        this.mouse_x = document.querySelector(".square").style.left;
-        this.mouse_y = document.querySelector(".square").style.top;
+        this.mouse_x = document.querySelector(".square").offsetLeft;
+        this.mouse_y = document.querySelector(".square").offsetTop;
+        this.count = false;
+        this.speed = 5;
         this.add_event();
     }
     
     // Event Listener
     add_event() {
-        this.box.addEventListener("mousemove", this.get_coords.bind(this));
+        this.box.addEventListener("mousemove",this.get_coords.bind(this));
+        window.addEventListener("mousemove",this.target_detector.bind(this));
+        setInterval(this.speed_accelerator.bind(this), 1000);
+    }
+
+    // Get coordinates from mousemove event, call box delimiter function, 
+    // and update coords with interval
+    get_coords(event) {
+        this.mouse_x = event.clientX;
+        this.mouse_y = event.clientY;
+        this.count = true;
+        this.box_delimiter();
+        this.update();
+    }
+
+    // update new coordinates to the square
+    update() {
+        this.square_el.style.left =  this.mouse_x + 'px',
+        this.square_el.style.top =  this.mouse_y + 'px';
+        this.square_el.style.transition = this.speed + "s";
+    }
+
+    // Detect when mouse is inside or outside the target
+    target_detector(event) {
+        if (event.clientX <= this.margin_left + this.box_width && event.clientX >= this.margin_left &&
+            event.clientY <= this.margin_bottom && event.clientY >= this.margin_top) {
+                this.count = true;
+            }
+            else {
+                this.count = false;
+            }
     }
 
     // Block mouse coordinates in each direction
@@ -38,25 +70,16 @@ class Square_run {
         }
     }
     
-    // update new coordinates to the square
-    update() {
-        this.square_el.style.position = "absolute";
-        this.square_el.style.left =  this.mouse_x + 'px',
-        this.square_el.style.top =  this.mouse_y + 'px';
-        this.square_el.style.transition = "2s";
-    }
+    // Manage square speed based on mouse coordinates
+    speed_accelerator() {
+        if (!this.count && this.speed <= 10) {
+            this.speed+=1;
+        } 
+        else if (this.speed > 1){
+            this.speed-=1;
+        }
+    }e
     
-    // Get coordinates from mousemove event, call box delimiter function, 
-    // and update coords with interval
-    get_coords(event) {
-        this.mouse_x = event.clientX;
-        this.mouse_y = event.clientY;
-        this.box_delimiter();
-        this.update();
-    } 
-
-      
 }
 
 new Square_run();
-
