fa568562bc9536aa2a04769a511e615055075c54
Simone <63718887+Srizza93@users.noreply.github.com>
2021-02-15 23:17:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class Square_run {
         this.mouse_x = document.querySelector(".square").offsetLeft;
         this.mouse_y = document.querySelector(".square").offsetTop;
         this.count = false;
-        this.speed = 5;
+        this.speed = 1.5;
         this.add_event();
     }
     
@@ -21,6 +21,14 @@ class Square_run {
         window.addEventListener("mousemove",this.target_detector.bind(this));
         setInterval(this.speed_accelerator.bind(this), 1000);
     }
+    
+    // Update new coordinates to the square
+    update() {
+        this.square_el.style.left =  this.mouse_x + 'px',
+        this.square_el.style.top =  this.mouse_y + 'px';
+        this.square_el.style.transition = `all ${this.speed}s`;
+        window.requestAnimationFrame(this.update.bind(this));
+    }
 
     // Get coordinates from mousemove event, call box delimiter function, 
     // and update coords with interval
@@ -29,14 +37,7 @@ class Square_run {
         this.mouse_y = event.clientY;
         this.count = true;
         this.box_delimiter();
-        this.update();
-    }
-
-    // Update new coordinates to the square
-    update() {
-        this.square_el.style.left =  this.mouse_x + 'px',
-        this.square_el.style.top =  this.mouse_y + 'px';
-        this.square_el.style.transition = `all ${this.speed}s`;
+        window.requestAnimationFrame(this.update.bind(this));
     }
 
     // Detect when mouse is inside or outside the target
@@ -70,13 +71,14 @@ class Square_run {
         }
     }
 
-    // Manage square speed based on mouse coordinates
+    // If square is in the box increase speed up to 10s, 
+    // else decrease it to minimum 1s
     speed_accelerator() {
-        if (!this.count && this.speed < 10) {
-            this.speed+=1;
+        if (!this.count && this.speed < 3) {
+            this.speed+=0.1;
         } 
-        if (this.count && this.speed > 1){
-            this.speed-=1;
+        if (this.count && this.speed > 0.1){
+            this.speed-=0.1;
         }
     }
     
